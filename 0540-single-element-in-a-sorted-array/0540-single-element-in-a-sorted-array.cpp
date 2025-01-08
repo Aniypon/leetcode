7291bd7615b0048990bdf421ed815c13f72081f2
Aniy <Arseniypon06@gmail.com>
2025-01-08 14:10:07 +0300

    Time: 0 ms (100%), Space: 26.3 MB (26.28%) - LeetHub

---
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int l = 0, r = nums.size()-1, mid;
+        while (l < r){
+            mid = (r-l)/2 + l;
+
+            if (mid % 2 != 0){
+                mid --;
+            }
+
+            if (nums[mid] != nums[mid+1]){
+                r = mid;
+            }
+            else{
+                l = mid +2;
+            }
+        }
+        return nums[l];
+    }
+};
